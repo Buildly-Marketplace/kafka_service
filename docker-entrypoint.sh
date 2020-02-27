846c0861c508c86db2018347ce44526b1dadb658
Jeferson Moura <jefmmoreira@gmail.com>
2020-02-27 16:12:52 +0100

    Update docker files and scripts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# It is responsability of the deployment orchestration to execute before
-# migrations, create default admin user, populate minimal data, etc.
+set -e
 
-gunicorn kafka_config.wsgi --config kafka_config/gunicorn_conf.py
+bash scripts/wait-for-it.sh $DATABASE_HOST $DATABASE_PORT
+
+echo $(date -u) "- Migrating"
+python manage.py migrate
+
+echo $(date -u) "- Creating admin user"
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+
+echo $(date -u) "- Running the server"
+gunicorn kakfaservice.wsgi --config kakfaservice/gunicorn_conf.py
