d483003a7c999aea0158a23f67dbe673579162d8
Jeferson Moura <jefmmoreira@gmail.com>
2020-02-27 16:20:10 +0100

    Create kafka view to interact with cluster

--- kafka_service/views.py
+++ kafka_service/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.conf import settings
+
+from rest_framework import status, views
+from rest_framework.response import Response
+
+from kafka_service.client import KafkaClient
+
+
+class KafkaServer(views.APIView):
+    def post(self, request, *args, **kwargs):
+        client = KafkaClient(settings.KAFKA_TOPIC)
+
+        message = json.dumps(request.data)
+        client.publish(message)
+
+        return Response(status=status.HTTP_201_CREATED)
+
+    def get(self, request, *args, **kwargs):
+        client = KafkaClient(settings.KAFKA_TOPIC)
+        messages = client.consume()
+
+        return Response(messages, status=status.HTTP_200_OK)
