61b071af5bf6e96ec1b2a3661544033009e6c88c
Jeferson Moura <jefmmoreira@gmail.com>
2020-02-27 16:14:48 +0100

    Update kafka config package

--- kafka_config/urls.py
+++ kafka_config/urls.py
@@ -1,21 +1,6 @@
-"""kafka_config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path, include, re_path
+from django.urls import path, re_path
 from rest_framework import permissions
 
 
@@ -26,7 +11,7 @@ from drf_yasg import openapi
 swagger_info = openapi.Info(
         title="Kafka Service",
         default_version='latest',
-        description="A service to connect to Kafka clus",
+        description="A service for connection to a Kafka instance",
 )
 
 schema_view = get_schema_view(
@@ -38,13 +23,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        re_path(r'^docs/swagger(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0),
-         name='schema-swagger-ui'),
-    path('health_check/', include('health_check.urls')),
-
+    re_path(r'^docs/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
-
