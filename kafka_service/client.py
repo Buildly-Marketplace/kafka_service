80261b98575293045fcaacc48bb9216e0fee4a62
Jeferson Moura <jefmmoreira@gmail.com>
2020-03-02 18:43:42 +0100

    Update Kafka client class

--- kafka_service/client.py
+++ kafka_service/client.py
@@ -7,25 +7,31 @@ from kafka import KafkaConsumer, KafkaProducer
 class KafkaClient:
     def __init__(self, topic: str):
         self.__topic = topic
-        self.__server_url = f'{settings.KAFKA_HOST}:{settings.KAFKA_PORT}'
+        self.__servers = settings.KAFKA_HOSTS
 
         self.__producer = KafkaProducer(
-            bootstrap_servers=[self.__server_url],
+            bootstrap_servers=self.__servers,
             retries=5
         )
 
     def publish(self, payload: dict, key: bytes = None, headers: list = None):
         body = json.dumps(payload).encode('utf-8')
 
-        self.__producer.send(
+        future = self.__producer.send(
             topic=self.__topic,
             key=key if key else self.__topic.encode('utf-8'),
             value=body
         )
 
+        future.get(timeout=60)
+        self.__producer.flush(timeout=60)
+
     def consume(self):
-        return KafkaConsumer(
-            self.__topic,
-            bootstrap_servers=[self.__server_url],
-            consumer_timeout_ms=1000
+        consumer = KafkaConsumer(
+            bootstrap_servers=self.__servers,
+            consumer_timeout_ms=60
         )
+        consumer.subscribe(topics=[self.__topic])
+        consumer.topics()
+        consumer.seek_to_beginning()
+        return consumer
